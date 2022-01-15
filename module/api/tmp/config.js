54a78970ac465dd925be123235726c3d657b2b86
Dida putra aditiya <didaputraa@gmail.com>
2022-01-15 19:00:33 +0700

    remove tmp asset

--- module/api/tmp/config.js
+++
@@ -1,8 +0,0 @@
-let current = 'http://localhost/testing/working/native2-2/';
-
-const api = {
-	url : current,
-	get : current + 'module/api/get.php',
-	store : current + 'module/api/store.php',
-	delete: current + 'module/api/remove.php?id='
-};
